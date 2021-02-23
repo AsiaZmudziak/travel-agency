423ae5102bcff8ffd1563b6f9c303fbc7c726057
unknown <asiazmudziak@gmail.com>
2021-02-23 23:49:31 +0100

    displaying the calculated price

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styles from './OrderForm.scss';
+//import styles from './OrderForm.scss';
 import {Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 
