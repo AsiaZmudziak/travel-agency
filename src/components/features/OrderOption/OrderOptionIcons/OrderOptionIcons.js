abd612dcf0c05d5430e681a4f50e4bdf83478091
unknown <asiazmudziak@gmail.com>
2021-02-24 23:10:26 +0100

    add dropdown, icons, number, checkboxes components

--- src/components/features/OrderOption/OrderOptionIcons/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons/OrderOptionIcons.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from '../OrderOption.scss';
+import Icon from '../../../common/Icon/Icon';
+import { formatPrice } from '../../../../utils/formatPrice';
 
-const OrderOptionIcons = props => <div>{props.name}</div>;
+const OrderOptionIcons = ({ required, values, currentValue, setOptionValue }) => (
+  <div className={styles.icon}>
+    {required ? (
+      ''
+    ) : (
+      <div className={styles.icon} onClick={() => setOptionValue()}>
+        <Icon name="times-circle" />
+        None
+      </div>
+    )}
+    {values.map(value => (
+      <div
+        className={`${styles.icon} ${
+          currentValue === value.id ? styles.iconActive : ''
+        }`}
+        key={value.id}
+        onClick={() => setOptionValue(value.id)}
+      >
+        <Icon name={value.icon} />
+        {value.name} ({formatPrice(value.price)})
+      </div>
+    ))}
+  </div>
+);
 
 OrderOptionIcons.propTypes = {
-  name: PropTypes.string,
+  values: PropTypes.array,
+  required: PropTypes.bool,
+  currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
 };
 
 export default OrderOptionIcons;
