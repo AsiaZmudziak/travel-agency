979e6c9196e62807e5347c0637d8f80765b6751f
unknown <asiazmudziak@gmail.com>
2021-02-24 19:55:15 +0100

    Option type components

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 //import styles from './OrderForm.scss';
-import { Row, Col } from 'react-flexbox-grid';
+
+import {Row, Col} from 'react-flexbox-grid';
+
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
 
 const OrderForm = props => (
   <Row>
-    {pricing.map((option, setOrderOption) => (
+    {pricing.map(option => (
       <Col md={4} key={option.id}>
         <OrderOption {...option} 
           currentValue={props.options[option.id]}
-          setOrderOption={setOrderOption}
+          setOrderOption={props.setOrderOption}
         />
       </Col>
     ))}
@@ -25,5 +27,6 @@ const OrderForm = props => (
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
+  setOrderOption: PropTypes.func,
 };
 export default OrderForm;
