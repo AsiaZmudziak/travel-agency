54bff16d91594ba5464185ee503328da5e350693
unknown <asiazmudziak@gmail.com>
2021-02-24 18:51:51 +0100

    Props in the options component

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 //import styles from './OrderForm.scss';
-import {Row, Col} from 'react-flexbox-grid';
+import { Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
-
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
 
-const OrderForm = ({tripCost, options}) => (
+const OrderForm = props => (
   <Row>
-    {pricing.map(option => (
+    {pricing.map((option, setOrderOption) => (
       <Col md={4} key={option.id}>
-        <OrderOption {...option} />
+        <OrderOption {...option} 
+          currentValue={props.options[option.id]}
+          setOrderOption={setOrderOption}
+        />
       </Col>
     ))}
     <Col xs={12}>
-      <OrderSummary tripCost={tripCost} options={options}/>
+      <OrderSummary tripCost={props.tripCost} options={props.options} />
     </Col>
   </Row>
 );
-
 
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
