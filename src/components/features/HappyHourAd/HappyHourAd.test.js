6d10b9250aeacc0e2b46cfc271eed8aa75b727ac
unknown <asiazmudziak@gmail.com>
2021-03-11 19:58:28 +0100

    Test 2: Are both items rendered?

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import HappyHourAd from './HappyHourAd';
-//import { processNodes } from 'react-html-parser';
+
+const select = {
+  title: '.title',
+  countdown: '.countdown',
+};
 
 describe('Component HappyHourAd', () => {
   it('should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();
-  }); 
+  });
+
+  it('should render heading and description', () => {
+    const component = shallow(<HappyHourAd />);
+    expect(component.exists(select.title)).toEqual(true);
+    expect(component.exists(select.countdown)).toEqual(true);
+  });
 });
